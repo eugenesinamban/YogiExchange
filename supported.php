9115c5ae5af6703175781e13bc763a6c459d1e79
eugenesinamban <49954579+eugenesinamban@users.noreply.github.com>
2020-01-20 07:58:27 +0900

    new commit

--- supported.php
+++ supported.php
@@ -1,16 +1,20 @@
 <?php 
 require("class/Output.php"); 
+require("class/Rates.php");
 require("bootstrap.php");
 date_default_timezone_set("asia/tokyo");
 $template = $twig->load("supported.twig");
 // 
 $output = new Output();
+$rates = new Rates();
 // 
 $viewVars = [
     // 
     'url' => basename($_SERVER['PHP_SELF']),
     'filetime' => date("Y-m-j h:i" ,filemtime("rates.json")),
-    'output' => $output
+    'output' => $output,
+    'rates' => $rates
+    
 ];
 // 
 echo $template->render($viewVars);
